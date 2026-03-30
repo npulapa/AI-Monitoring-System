4b061186fc261d86bc57ffb2791a19f305489434
npulapa <pulapanandini14@gmail.com>
2026-03-30 14:43:25 +0530

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -1,1 +1,20 @@
+def evaluate_answer(answer):
+    correct_answer = "Paris is the capital of France"
 
+    if answer.strip().lower() == correct_answer.lower():
+        return {
+            "score": "correct",
+            "feedback": "Good job!"
+        }
+    else:
+        return {
+            "score": "wrong",
+            "feedback": "Try again."
+        }
+
+
+# Run program
+if __name__ == "__main__":
+    user_answer = input("Enter your answer: ")
+    result = evaluate_answer(user_answer)
+    print(result)
