1f4d5590b216364b06e77b4212b3914dea6a628b
npulapa <pulapanandini14@gmail.com>
2026-03-30 14:07:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
 
+app = FastAPI()
+
+class Data(BaseModel):
+    name: str
+    score: int
+
+@app.get("/test")
+def test():
+    return {"message": "API working"}
+
+@app.post("/submit")
+def submit(data: Data):
+    return {
+        "status": "received",
+        "name": data.name,
+        "score": data.score
+    }
